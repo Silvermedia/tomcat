9cc810f48ad5730c22358b74a851e8fff60aeaca
Tomasz Stańda <tomasz.standa@silvermedia.pl>
2020-01-02 10:09:04 +0100

    Move curl instalation to base image

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
-VERSION=8.5.49-jdk11-openjdk-slim
+BASE_IMAGE_VERSION=8.5.49-jdk11-openjdk-slim
+VERSION=8.5.49-jdk11-openjdk-slim-1
 
-docker build -f ./DockerfileTomcat --build-arg VERSION=$VERSION -t silvermedia/tomcat:$VERSION .
+docker build -f ./DockerfileTomcat --build-arg VERSION=$BASE_IMAGE_VERSION -t silvermedia/tomcat:$VERSION .
 docker build -f ./DockerfileTomcatDCEVM --build-arg VERSION=$VERSION -t silvermedia/tomcat-dcevm:$VERSION .
