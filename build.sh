198877b26d6ce7124095df6ab754c20ce202fa29
Michał Grad <khaine@silvermedia.pl>
2020-12-02 11:41:45 +0100

    TK-1324 Podniesiona wersja w celu ponownego wydania obrazu
    Poprzednia wersja zawierała błędnie otagowane wariant DCEVM jako oba obrazy.

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 BASE_IMAGE_VERSION=8.5.60-jdk11-openjdk-slim
-VERSION=8.5.60-jdk11-openjdk-slim
+VERSION=8.5.60-jdk11-openjdk-slim.sm2
 
 docker build -f ./DockerfileTomcat --build-arg VERSION=$BASE_IMAGE_VERSION -t silvermedia/tomcat:$VERSION .
 docker build -f ./DockerfileTomcatDCEVM --build-arg VERSION=$VERSION -t silvermedia/tomcat-dcevm:$VERSION .
